4ec54f3ddbb3d23bd87d26b459cf6d0561a8d462
GesS016 <gusikasadov@gmail.com>
2022-11-23 01:48:52 +0400

    ClientRegistration method added

--- CleaningBackTesting/Client/SuperClient.cs
+++ CleaningBackTesting/Client/SuperClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleaningBackTesting.RequestModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace CleaningBackTesting.Client
 {
-    internal class SuperClient
+    public class SuperClient
     {
+        public string ClientRegistration(ClientRegistrationRequestModel clientRegistrationRequestModel)
+        {
+
+        }
     }
 }
