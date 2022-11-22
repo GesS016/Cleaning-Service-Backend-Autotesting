2e447b33840b3e2c7e5f4f3087cc7a5f229c91ba
nigar <nigar-djafarova@mail.ru>
2022-11-22 14:34:37 +0400

    new example method

--- CleaningBackTesting/RequestModels/AuthRequestModel.cs
+++ CleaningBackTesting/RequestModels/AuthRequestModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CleaningBackTesting.RequestModels
 {
-    internal class AuthRequestModel
+    public class AuthRequestModel
     {
+        public static string aftosh(string d, string x)
+        {
+            return $"{d}+{x}";
+        }
     }
 }
