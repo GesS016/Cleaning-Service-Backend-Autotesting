bf8550416bb7f90de16f2709f506fde5d26015dc
sayojj <aleksbaydikov@gmail.com>
2022-11-22 14:25:19 +0400

    Models added

---
+++ CleaningBackTesting/RequestModels/AuthRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningBackTesting.RequestModels
+{
+    internal class AuthRequestModel
+    {
+    }
+}
