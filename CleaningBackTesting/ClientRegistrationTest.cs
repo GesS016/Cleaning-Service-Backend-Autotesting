9903d22eb97700a184c2c3f14cc6679ef4637032
sayojj <aleksbaydikov@gmail.com>
2022-11-23 03:34:09 +0400

    Client Registration Test small changes

--- CleaningBackTesting/ClientRegistrationTest.cs
+++ CleaningBackTesting/ClientRegistrationTest.cs
@@ -6,22 +6,24 @@ using NUnit.Framework;
 namespace CleaningBackTesting
 {
     public class ClientRegistrationTest
-{
+    {
 
         [Test]
-    public void Test1()
-    {
-        ClientRegistrationRequestModel clientRegistrationRequestModel = new ClientRegistrationRequestModel()
+        public void Test1()
         {
-            FirstName = "Luke",
-            LastName = "Skywalker",
-            BirthDate = "1971-03-14T10:47:35.733Z",  //id 406
-            Password = "stringst",
-            ConfirmPassword = "stringst",
-            Email = "lukesky@example.com",
-            Phone = "string"
-        };
-        SuperClient client = new SuperClient();
-        string id = client.ClientRegistration(clientRegistrationRequestModel);
+            ClientRegistrationRequestModel clientRegistrationRequestModel = new ClientRegistrationRequestModel()
+            {
+                FirstName = "Luke",
+                LastName = "Skywalker",
+                BirthDate = "1971-03-14T10:47:35.733Z",  //id 406
+                Password = "stringst",
+                ConfirmPassword = "stringst",
+                Email = "lukesky@example.com",
+                Phone = "string"
+            };
+            SuperClient client = new SuperClient();
+            string id = client.ClientRegistration(clientRegistrationRequestModel);
+            //Assert.AreEqual(clientRegistrationRequestModel.Equals(clientRegistrationRequestModel), true);
+        }
     }
 }
