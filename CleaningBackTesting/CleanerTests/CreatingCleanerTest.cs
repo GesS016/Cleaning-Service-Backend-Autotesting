92dfcf461779de753a75091d4dda33a0eb258a10
nigar <nigar-djafarova@mail.ru>
2022-11-24 19:34:23 +0400

    Cleaner Creating Test

---
+++ CleaningBackTesting/CleanerTests/CreatingCleanerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CleaningBackTesting.Client;
+using CleaningBackTesting.RequestModels;
+using NUnit.Framework;
+
+
+namespace CleaningBackTesting.CleanerTests
+{
+    public class CreatingCleanerTest
+    {
+
+        [Test]
+        public void Test1()
+        {
+            CleanerRegistrationRequestModel cleanerRegistrationRequestModel = new CleanerRegistrationRequestModel()
+            {
+                FirstName = "Luke",
+                LastName = "Skywalker",
+                BirthDate = "1971-03-14T10:47:35.733Z",  //id 406
+                Password = "stringst",
+                ConfirmPassword = "stringst",
+                Email = "lukesky@example.com",
+                Phone = "string",
+                Passport = "stringstri",
+                Schedule = 1,
+                ServicesIds = { 0 }, //Maks pomogi
+                Districts = { 1 }
+            };
+            Client.CleanerClient cleanerclient = new Client.CleanerClient();
+            //string id = cleanerclient.CleanerRegistration(CleanerRegistrationRequestModel);
+            //Assert.AreEqual(clientRegistrationRequestModel.Equals(clientRegistrationRequestModel), true);
+        }
+    }
+}
---
+++ CleaningBackTesting/RequestModels/CleanerRegistrationRequestModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CleaningBackTesting.RequestModels
+{
+    public class CleanerRegistrationRequestModel
+    {
+    
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("birthDate")]
+        public string BirthDate { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("confirmPassword")]
+        public string ConfirmPassword { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("passport")]
+        public string Passport { get; set; }
+
+        [JsonPropertyName("schedule")]
+        public int Schedule { get; set; }
+
+        [JsonPropertyName("servicesIds")]
+        public List<int> ServicesIds { get; set; }
+
+        [JsonPropertyName("districts")]
+        public List<int> Districts { get; set; }
+    }
+}
