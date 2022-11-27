3b49b4e7e788fc630ef86bf62d9813ca829265a4
GesS016 <gusikasadov@gmail.com>
2022-11-27 04:38:46 +0400

    client registr test modified, Clients changed

--- CleaningBackTesting/Client/CleanerClient.cs
+++ CleaningBackTesting/Client/CleanerClient.cs
@@ -35,5 +35,27 @@ namespace CleaningBackTesting.Client
 
             return id;
         }
+
+        public string Auth(AuthRequestModel cleanerAuthRequestModel)
+        {
+            HttpStatusCode expectedCode = HttpStatusCode.OK;
+            string json = JsonSerializer.Serialize<AuthRequestModel>(cleanerAuthRequestModel);
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(clientHandler);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri($"https://piter-education.ru:10042/Auth"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage responseMessage = client.Send(message);
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+            Assert.AreEqual(expectedCode, actualCode);
+
+            string token = responseMessage.Content.ReadAsStringAsync().Result;
+
+            return token;
+        }
     }
 }
