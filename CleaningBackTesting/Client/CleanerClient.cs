0f7d5844e7f0622b1bd3321cbea1280f5419e5b7
sayojj <aleksbaydikov@gmail.com>
2022-11-27 04:37:48 +0400

    Cleaner list first add

--- CleaningBackTesting/Client/CleanerClient.cs
+++ CleaningBackTesting/Client/CleanerClient.cs
@@ -1,8 +1,10 @@
-﻿using CleaningBackTesting.RequestModels;
+﻿using CleaningBackTesting.Models.ResponseModels;
+using CleaningBackTesting.RequestModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -34,6 +36,32 @@ namespace CleaningBackTesting.Client
             int id = Convert.ToInt32(responseMessage.Content.ReadAsStringAsync().Result);
 
             return id;
+        }
+        public List<CleanerListResponseModel> GetCleaners(string token)
+        {
+            HttpStatusCode expectedCode = HttpStatusCode.OK;
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new System.Uri($"https://piter-education.ru:10042/Cleaners"),
+            };
+
+            HttpResponseMessage responseMessage = client.Send(message);
+
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+            Assert.AreEqual(expectedCode, actualCode);
+
+            string responseJson = responseMessage.Content.ReadAsStringAsync().Result;
+            List<CleanerListResponseModel> cleaners = JsonSerializer.Deserialize<List<CleanerListResponseModel>>(responseJson)!;
+
+            return cleaners;
         }
     }
 }
