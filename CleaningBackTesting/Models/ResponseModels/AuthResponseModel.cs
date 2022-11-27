8cee50fe936b02dec6c4edf515231bbea611e3f8
Mina <minaahmedova1996@gmail.com>
2022-11-28 03:35:50 +0400

    Add Auth responce model

--- CleaningBackTesting/Models/ResponseModels/AuthResponseModel.cs
+++ CleaningBackTesting/Models/ResponseModels/AuthResponseModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using CleaningBackTesting.RequestModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CleaningBackTesting.ResponseModels
 {
-    internal class AuthResponseModel
+    public class AuthResponseModel
     {
-    }
+        public string Auth(AuthRequestModel adminAuthRequestModel);
+        HttpRespongeMessage({"string"});
+        ProducesResponseType(HttpStatusCode.Status200.OK);
+        ProducesResponseType(HttpStatusCode.Status200.Success);
+
+        
+    };
 }
