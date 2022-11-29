6df1a7fb41524ba82c969421243c9c45a4c3f1aa
nigar <nigar-djafarova@mail.ru>
2022-11-29 17:20:46 +0400

    Add Service request model

---
+++ CleaningBackTesting/Models/RequestModels/ServiceRequestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CleaningBackTesting.Models.RequestModels
+{
+    public class ServiceRequestModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("roomType")]
+        public int RoomType { get; set; }
+
+        [JsonPropertyName("price")]
+        public int Price { get; set; }
+
+        [JsonPropertyName("unit")]
+        public string Unit { get; set; }
+
+        [JsonPropertyName("duration")]
+        public int Duration { get; set; }
+    }
+}
+
