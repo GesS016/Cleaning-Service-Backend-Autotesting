8e815568a0ae6182686b0a0212df35a792243fb1
nigar <nigar-djafarova@mail.ru>
2022-11-29 17:41:12 +0400

    Add order request

---
+++ CleaningBackTesting/Models/RequestModels/OrdersRequestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CleaningBackTesting.Models.RequestModels
+{
+    public class OrdersRequestModel
+    {
+        [JsonPropertyName("clientId")]
+        public int ClientId { get; set; }
+
+        [JsonPropertyName("cleaningObjectId")]
+        public int CleaningObjectId { get; set; }
+
+        [JsonPropertyName("startTime")]
+        public DateTime StartTime { get; set; }
+
+        [JsonPropertyName("bundlesIds")]
+        public List<int> BundlesIds { get; set; }
+
+        [JsonPropertyName("servicesIds")]
+        public List<int> ServicesIds { get; set; }
+    }
+}
+
