34d6b95ecec00c6046cdb1c67c1e9aae38aad537
GesS016 <gusikasadov@gmail.com>
2022-12-02 01:38:45 +0400

    CreateOrder method added

--- CleaningBackTesting/Client/ClientClient.cs
+++ CleaningBackTesting/Client/ClientClient.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
@@ -13,6 +14,8 @@ namespace CleaningBackTesting.Client
 {
     public class ClientClient
     {
+        private const string HOST = "https://piter-education.ru:10042";
+        private const string CLIENTSHOST = HOST + "/Clients";
         public int ClientRegistration(ClientRegistrationRequestModel clientRegistrationRequestModel)
         {
             HttpStatusCode expectedCode = HttpStatusCode.Created; //ожидаемый код=201
@@ -62,22 +65,12 @@ namespace CleaningBackTesting.Client
             return token;
         }
 
-        public int CreateOrder(BundlesRequestModel ordersRequestModel)
+        public int CreateOrder(OrdersRequestModel ordersRequestModel, string token)
         {
             HttpStatusCode expectedCode = HttpStatusCode.Created; 
-            string json = JsonSerializer.Serialize<BundlesRequestModel>(ordersRequestModel);
+            string json = JsonSerializer.Serialize<OrdersRequestModel>(ordersRequestModel);
            
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-            
-            HttpClient client = new HttpClient(clientHandler);
-            HttpRequestMessage message = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Post, 
-                RequestUri = new System.Uri($"https://piter-education.ru:10042/Orders"),
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
-            };
-            HttpResponseMessage responseMessage = client.Send(message);
+            HttpResponseMessage responseMessage = SendRequest(HttpMethod.Post, CLIENTSHOST, token, json);
 
             HttpStatusCode actualCode = responseMessage.StatusCode;
             Assert.AreEqual(expectedCode, actualCode);
@@ -85,6 +78,31 @@ namespace CleaningBackTesting.Client
             int id = Convert.ToInt32(responseMessage.Content.ReadAsStringAsync().Result);
 
             return id;
+        }
+
+        private static HttpResponseMessage SendRequest(HttpMethod httpMethod, string uriString, string token = null, string jsonContent = null)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = httpMethod,
+                RequestUri = new System.Uri(uriString)
+            };
+            if (!string.IsNullOrWhiteSpace(jsonContent))
+            {
+                message.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            }
+
+            HttpResponseMessage responseMessage = client.Send(message);
+            return responseMessage;
         }
     }
 }
