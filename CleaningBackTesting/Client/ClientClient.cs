2a2dd7f8387e00191343f9b51d5cc0d6e7e7c335
sayojj <aleksbaydikov@gmail.com>
2022-12-02 04:42:39 +0400

    Client Auth method recycling <3
    <3<3<3<3<3<3<3<3<3<3

--- CleaningBackTesting/Client/ClientClient.cs
+++ CleaningBackTesting/Client/ClientClient.cs
@@ -46,19 +46,12 @@ namespace CleaningBackTesting.Client
         public string Auth(AuthRequestModel clientAuthRequestModel)
         {
             HttpStatusCode expectedCode = HttpStatusCode.OK;
-            string json = JsonSerializer.Serialize<AuthRequestModel>(clientAuthRequestModel);
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-            HttpClient client = new HttpClient(clientHandler);
-            HttpRequestMessage message = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Post,
-                RequestUri = new System.Uri($"https://piter-education.ru:10042/Auth"),
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
-            };
-            HttpResponseMessage responseMessage = client.Send(message);
-            //HttpStatusCode actualCode = responseMessage.StatusCode; //???
-            //Assert.AreEqual(expectedCode, actualCode);
+            string json = JsonSerializer.Serialize<AuthRequestModel>(adminAuthRequestModel);
+
+            HttpResponseMessage responseMessage = SendRequest(HttpMethod.Post, HOST + "/auth", jsonContent: json);
+            HttpStatusCode actualCode = responseMessage.StatusCode;
+
+            Assert.AreEqual(expectedCode, actualCode);
 
             string token = responseMessage.Content.ReadAsStringAsync().Result;
 
