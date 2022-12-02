fbee9b7f5caac5b4a14f13182882f7a689570af2
Mina <minaahmedova1996@gmail.com>
2022-12-02 17:12:12 +0400

    Send request clients

---
+++ CleaningBackTesting/Client/SendRequestForClients.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningBackTesting.Client
+{
+    public class SendRequestForClients
+    {
+        public static HttpResponseMessage SendRequest(HttpMethod httpMethod, string uriString, string token = null, string jsonContent = null)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = httpMethod,
+                RequestUri = new System.Uri(uriString)
+            };
+            if (!string.IsNullOrWhiteSpace(jsonContent))
+            {
+                message.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            }
+
+            HttpResponseMessage responseMessage = client.Send(message);
+            return responseMessage;
+        }
+    }
+}
