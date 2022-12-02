cc1b7b503a7c91147201b95751cd4e0bda4e50c1
sayojj <aleksbaydikov@gmail.com>
2022-12-02 04:28:13 +0400

    Admin Auth method recycling

--- CleaningBackTesting/Client/AdminClient.cs
+++ CleaningBackTesting/Client/AdminClient.cs
@@ -20,17 +20,10 @@ namespace CleaningBackTesting.Client
         {
             HttpStatusCode expectedCode = HttpStatusCode.OK;
             string json = JsonSerializer.Serialize<AuthRequestModel>(adminAuthRequestModel);
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-            HttpClient client = new HttpClient(clientHandler);
-            HttpRequestMessage message = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Post,
-                RequestUri = new System.Uri($"https://piter-education.ru:10042/Auth"),
-                Content = new StringContent(json, Encoding.UTF8, "application/json")
-            };
-            HttpResponseMessage responseMessage = client.Send(message);
+
+            HttpResponseMessage responseMessage = SendRequest(HttpMethod.Post, HOST + "/auth", jsonContent : json);
             HttpStatusCode actualCode = responseMessage.StatusCode;
+
             Assert.AreEqual(expectedCode, actualCode);
 
             string token = responseMessage.Content.ReadAsStringAsync().Result;
